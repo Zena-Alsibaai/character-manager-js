a458d07cfb06e3fd90640738c176dafb75d00fff
Zena-Alsibaai <zena.alsibaai@gmail.com>
2021-03-02 15:34:34 +0100

    Add modal

--- src/js/main.js
+++ src/js/main.js
@@ -1,35 +1,37 @@
-
-
-
-
 (() => {
-    // your code here
-    const template = document.querySelector('#template')
-    const target = document.querySelector("#target")
+  // your code here
+  const template = document.querySelector("#template");
+  const target = document.querySelector("#target");
 
-    async function getData() {
+  async function getData() {
+    const response = await fetch(
+      "https://character-database.becode.xyz/characters"
+    );
+    const heroes = await response.json(); //transforme le format de l'url en format json pour pouvoir le lire
 
+    console.log(heroes);
+    //console.log(heroes[0].image)
 
-        const response = await fetch("https://character-database.becode.xyz/characters")
-        const heroes = await response.json() //transforme le format de l'url en format json pour pouvoir le lire
+    heroes.forEach(({ name, shortDescription, image }) => {
+      const clone = template.cloneNode(true).content;
+      const img = "data:image/jpeg;base64," + image;
+      console.log(img);
 
-        console.log(heroes)
-        //console.log(heroes[0].image)
+      clone.querySelector(".card-img-top").src = img;
+      clone.querySelector(".card-title").textContent = name;
+      clone.querySelector(".card-text").textContent = shortDescription;
 
-        heroes.forEach(({ name, shortDescription, image }) => {
-            const clone = template.cloneNode(true).content;
-           const img = "data:image/jpeg;base64,"+image
-           console.log(img)
-            
+      target.appendChild(clone);
+    });
+  }
+  getData();
+  // Get the modal
+  let modal = document.getElementById("id01");
 
-            clone.querySelector(".card-img-top").src = img
-            clone.querySelector(".card-title").textContent = name
-            clone.querySelector(".card-text").textContent = shortDescription
-
-
-            target.appendChild(clone)
-        })
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
     }
-    getData()
-
-})()
+  };
+})();
